60a8170be0250c42d45aa129ae4bf9986236347a
irvingisc <irvingsalvador92gmail.com>
2022-02-03 21:20:24 -0600

    Component compound listo

---
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { ProductButtons, ProductCard, ProductImage, ProductTitle } from '../components';
+import {  } from '../components/ProductCard';
+import {  } from '../components/ProductButtons';
+
+
+const product = {
+    id: '1',
+    title: 'Coffee Mug - Card',
+    img:'./coffee-mug.png'
+}
+
+export const ShoppingPage = () => {
+    return (
+        <div>
+            <h1>Shopping Store</h1>
+            <hr />
+
+            <div style={{
+                display: 'flex',
+                flexDirection: 'row',
+                flexWrap: 'wrap'
+            }}>
+            
+                <ProductCard product={ product } >
+                    <ProductCard.Image  />
+                    <ProductCard.Title title={ 'Café' } />
+                    <ProductCard.Buttons />
+                </ProductCard>
+
+                <ProductCard product={ product } >
+                    <ProductImage />
+                    <ProductTitle />
+                    <ProductButtons />
+                </ProductCard>
+
+            </div>
+        </div>
+    )
+}
